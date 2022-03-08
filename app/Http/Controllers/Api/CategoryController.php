44f1b34f2ee029bb526809b2fed5d4d2d59b6064
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2022-03-08 18:44:17 -0400

    adicionado resource category

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+    }
+
+    public function show(Category $category)
+    {
+        //
+    }
+
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(["namespace" => "Api"], function () {
+    Route::apiResource("categories", "CategoryController");
+});
