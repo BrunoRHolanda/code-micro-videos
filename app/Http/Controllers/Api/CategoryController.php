397b43ddb41e69cf9c1a57397f0dfbfc24427af1
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2022-03-09 00:33:23 -0400

    feat: :sparkles: adicionado delete

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -33,6 +33,8 @@ class CategoryController extends Controller
 
     public function destroy(Category $category)
     {
-        //
+        $category->delete();
+
+        return response()->noContent();
     }
 }
