19fc46c6b9ed2821a733303cb620a28aee2817e2
Bruno R. Holanda <brunorodriguesholanda@gmail.com>
2022-08-15 14:56:09 -0400

    implementatos migration e model de cast member

--- app/Models/CastMember.php
+++ app/Models/CastMember.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CastMember extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Uuid;
+
+    protected $fillable = [
+        'name',
+        'type',
+    ];
+
+    protected $dates = ['deleted_at'];
+
+    protected $casts = [
+        'id' => 'string'
+    ];
+
+    public $incrementing = false;
 }
